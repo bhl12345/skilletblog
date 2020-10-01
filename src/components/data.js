66b17db2074f0218a7dce63339b2dd1b2132a952
bhl12345 <45795278+bhl12345@users.noreply.github.com>
2020-10-01 12:26:28 -0400

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -1,7 +1,7 @@
 // data.js
 const data = [
-  { _id: 1, author: 'Bryan', text: 'Wow this is neat', updatedAt: new Date(), createdAt: new Date() },
-  { _id: 2, author: 'You', text: 'You\'re __right!__', updatedAt: new Date(), createdAt: new Date() },
+  { _id: 1, author: 'Me', text: 'He is such a douche', updatedAt: new Date(), createdAt: new Date() },
+  { _id: 2, author: 'Me', text: 'Testing comment box', updatedAt: new Date(), createdAt: new Date() },
 ];
 
 export default data;
