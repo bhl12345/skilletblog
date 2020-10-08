0eb834239053fbcaa2ca4bd08a367b7b1310364b
bhl12345 <45795278+bhl12345@users.noreply.github.com>
2020-10-08 14:11:29 -0400

    connect server db

--- server/server.js
+++ server/server.js
@@ -14,22 +14,32 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://bhl12345:Me123-456@pontificatingpeasant.mkwnc.mongodb.net/pontificatingpeasant?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("pontificatingpeasant").collection("pontificatingpeasant");
+  const db = client.db("pontificatingpeasant");
+  const collection = db.collection("pontificatingpeasant");
   console.log('Connected to Database');
- 
-  client.close();
-});
-app.use(bodyParser.urlencoded({ extended: true })) 
-	
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-  // Note: __dirname is the current directory you're in. Try logging it and see what you get!
-})
-
-app.post('/', (req, res) => {
+  app.use(bodyParser.urlencoded({ extended: true })) 
+  app.post('/', (req, res) => {
+  collection.insertOne(req.body)
+    .then(result => {
+      console.log(result)
+    })
+    app.post('/pontificationpeasant', (req, res) => {
   collection.insertOne(req.body)
     .then(result => {
       console.log(result)
     })
     .catch(error => console.error(error))
 })
+  app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+})
+
+     
+})
+
+ 
+});
+
+	
+
