68f7e07f96c4a0489d9e9586bb4b4761064509dd
bhl12345 <45795278+bhl12345@users.noreply.github.com>
2020-11-30 14:21:24 -0500

    added audio

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -6,7 +6,7 @@ class AudioPlayer extends Component {
   render () {
   return (
     <div>
-      <h3>Listen To The - <a href="https://www.pontificating-peasant.com">Peasant Podcast</a></h3>
+      <h3>Coming Soon The - <a href="https://www.pontificating-peasant.com">Peasant Podcast</a></h3>
       <ReactPlayer
         url="https://github.com/bhl12345/skilletblog/blob/master/src/audio/01%20Show%20Me%20The%20Way%20(Live).mp3"
         width="545px"
