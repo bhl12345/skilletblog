d740645c6ea8a38117bbcc589e1c648a41db27e3
bhl12345 <45795278+bhl12345@users.noreply.github.com>
2020-12-07 15:58:09 -0500

    Update audioplayer.js

--- src/components/audioplayer.js
+++ src/components/audioplayer.js
@@ -5,7 +5,7 @@ import mp3_file from "../audio/peasantcast.mp3";
 function AudioPlayer() {
   return (
     <div>
-      <h3>Coming Soon The - <a style={{color:`#663399`}} href="https://www.pontificating-peaseant.com">Peasant Podcast</a></h3>
+      <h3>Click to listen to the - <a style={{color:`#663399`}} href="https://www.pontificating-peaseant.com">Peasant Podcast</a></h3>
       <ReactPlayer
         url={mp3_file}
         width="550px"
